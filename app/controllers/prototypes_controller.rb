a623e19fb57cdbcdf18e3c2bb211910df8cb9e8d
masaki-takahama <101110111+masaki-takahama@users.noreply.github.com>
2022-03-30 22:06:57 +0900

    微調整...

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,7 +30,7 @@ class PrototypesController < ApplicationController
   def edit
     @prototype = Prototype.find(params[:id])
     
-    unless @prototype.user_id == current_user
+    unless @prototype.user_id == current_user.id
       redirect_to action: :index
     end
   end
